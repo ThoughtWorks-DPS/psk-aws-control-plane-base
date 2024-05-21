bf1edaa2d988c4b6f0bb8cf91a2a3da717935ce8
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-05-21 18:07:34 -0500

    [nc] test write script
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/write_cluster_identifiers.sh
+++ scripts/write_cluster_identifiers.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source bash-functions.sh
+source bash_functions.sh
 set -eo pipefail
 
 cluster_name=$1
@@ -10,25 +10,12 @@ export AWS_REGION=$(jq -er .aws_region "$cluster_name".auto.tfvars.json)
 
 awsAssumeRole "$AWS_ACCOUNT_ID" "$AWS_ASSUME_ROLE"
 
-# update kubeconfig based on assume-role
+update kubeconfig based on assume-role
 aws eks update-kubeconfig --name "$cluster_name" \
 --region "$AWS_REGION" \
 --role-arn arn:aws:iam::"$AWS_ACCOUNT_ID":role/"$AWS_ASSUME_ROLE" --alias "$cluster_name" \
---kubeconfig "kubeconfig"
-#--kubeconfig "~/.kube/config"
+--kubeconfig "~/.kube/config"
 
-
-
-# cluster_role=$1
-
-# instance_name=$(jq -er .instance_name "$cluster_role".auto.tfvars.json)
-# #export AWS_REGION=us-east-1
-
-# cat ~/.kube/config
-
-# # write kubeconfig to AWS secrets manager
-# teller put KUBECONFIG_BASE64="$(cat ~/.kube/config | base64)" --providers aws_secretsmanager -c .teller.yml
-
-# # # write cluster url and pubic certificate to AWS secrets manager
-# teller put CLUSTER_URL="$(terraform output -raw cluster_url)" --providers aws_secretsmanager -c .teller.yml
-# teller put CLUSTER_PUBLIC_CERTIFICATE_AUTHORITY_DATA="$(terraform output -raw cluster_public_certificate_authority_data)" --providers aws_secretsmanager -c .teller.yml
+write1passwordField empc-lab $cluster_name kubeconfig-base64 $(cat ~/.kube/config | base64)
+write1passwordField empc-lab $cluster_name cluster-url $(terraform output -raw cluster_url)
+write1passwordField empc-lab $cluster_name base64-certificate-authority-data $(terraform output -raw cluster_public_certificate_authority_data)
