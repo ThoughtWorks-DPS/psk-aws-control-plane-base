76cbc161178f7d97a27af21e2d83807c4f163edf
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-05-27 12:35:14 -0500

    [nc] test initial karpenter node pool configuration
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/node_pool_tests.sh
+++ scripts/node_pool_tests.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 kubectl apply -f test/karpenter/amd-node-pool-deployment.yaml
 kubectl apply -f test/karpenter/arm-node-pool-deployment.yaml
 sleep 60
-bats apply -f test/karpenter/test-dynamic-node-pools.yaml
+bats test/karpenter/test-dynamic-node-pools.bats
 
 kubectl delete -f test/karpenter/amd-node-pool-deployment.yaml
 kubectl delete -f test/karpenter/arm-node-pool-deployment.yaml
