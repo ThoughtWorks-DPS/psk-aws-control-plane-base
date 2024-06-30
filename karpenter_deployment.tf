fb6f3b97a5af33cf8e1a1d4a1b04236de13b9335
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-06-30 14:34:37 -0500

    [nc] changing karpenter deployment to have dedicated crd management
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- karpenter_deployment.tf
+++ karpenter_deployment.tf
@@ -1,10 +1,23 @@
+# apply crd updates directly
+resource "helm_release" "karpenter-crd" {
+  namespace  = "kube-system"
+  name       = "karpenter-crd"
+  repository = "oci://public.ecr.aws/karpenter"
+  chart      = "karpenter-crd"
+  version    = var.karpenter_chart_version
+  wait       = true
+  values     = []
+}
+
 resource "helm_release" "karpenter" {
+  depends_on = [helm_release.karpenter-crd]
   namespace  = "kube-system"
   name       = "karpenter"
   repository = "oci://public.ecr.aws/karpenter"
   chart      = "karpenter"
   version    = var.karpenter_chart_version
-  wait       = false
+  wait       = true
+  skip_crds  = true
 
   values = [
     templatefile("tpl/karpenter_values.tpl", {
