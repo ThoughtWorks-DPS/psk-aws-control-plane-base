4640d6b728fa71aa754c2b4140bed8c209871abe
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-09-02 14:01:43 -0500

    [nc] added additional AwSpec tests to cover other primary resources created
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/node_pool_tests.sh
+++ scripts/node_pool_tests.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 # deploy test pod to default NodePools
 kubectl apply -f test/karpenter/amd-node-pool-deployment.yaml
 kubectl apply -f test/karpenter/arm-node-pool-deployment.yaml
-sleep 60
+sleep 120
 bats test/karpenter/test-dynamic-node-pools.bats
 
 kubectl delete -f test/karpenter/amd-node-pool-deployment.yaml
--- test/control_plane_base.rb
+++ test/control_plane_base.rb
@@ -9,10 +9,28 @@ describe eks(tfvars["cluster_name"]) do
   its(:version) { should eq tfvars['eks_version'] }
 end
 
+describe eks_nodegroup('my-eks-nodegroup'), cluster: 'my-cluster' do
+  it { should exist }
+  it { should be_ready }
+  its(:taints) { should include("dedicated" => { "effect" => "NO_SCHEDULE" }) }
+end
+
 describe iam_role(tfvars["cluster_name"] + '-vpc-cni') do
   it { should exist }
 end
 
 describe iam_role(tfvars["cluster_name"] + '-ebs-csi-controller-sa') do
+  it { should exist }
+end
+
+describe iam_role(tfvars["cluster_name"] + '-efs-csi-controller-sa') do
+  it { should exist }
+end
+
+describe efs(tfvars["cluster_name"] + "-efs-csi-storage") do
+  it { should exist }
+end
+
+describe sqs("Karpenter-" + tfvars["cluster_name"]) do
   it { should exist }
 end
